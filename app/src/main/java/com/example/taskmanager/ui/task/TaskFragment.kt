2d16c73b6059eb1be909537f28ce4a52f0787606
Akash <it22258380@my.sliit.lk>
2024-05-12 12:31:47 +0530

    app success up to delete

--- app/src/main/java/com/example/taskmanager/ui/task/TaskFragment.kt
+++ app/src/main/java/com/example/taskmanager/ui/task/TaskFragment.kt
@@ -4,11 +4,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.taskmanager.R
 import com.example.taskmanager.databinding.FragmentTaskBinding
 import com.example.taskmanager.viewmodel.TaskViewModel
+import com.google.android.material.snackbar.Snackbar
+
 class TaskFragment : Fragment() {
     private val viewModel:TaskViewModel by viewModels()
     private lateinit var adapter: TaskAdapter
@@ -33,6 +38,32 @@ class TaskFragment : Fragment() {
                 findNavController().navigate(R.id.action_taskFragment_to_addFragment)
             }
         }
+
+        ItemTouchHelper(object :ItemTouchHelper.SimpleCallback(0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+               val position=viewHolder.absoluteAdapterPosition
+                val taskEntry=adapter.currentList[position]
+                viewModel.delete(taskEntry)
+
+                Snackbar.make(binding.root,"Deleted",Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo"){
+                        viewModel.insert(taskEntry)
+                    }
+                    show()
+                }
+            }
+
+        }).attachToRecyclerView(binding.recyclerView)
+
         return binding.root
     }
 }
